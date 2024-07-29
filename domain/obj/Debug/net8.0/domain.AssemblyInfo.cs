771480f56f814b654743098eb94b4534668d6185
thatGui <thatguifonseca@gmail.com>
2024-07-29 15:12:17 +0100

    readded "" to connection string

--- domain/obj/Debug/net8.0/domain.AssemblyInfo.cs
+++ domain/obj/Debug/net8.0/domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfba3857a22ce33a30eeb45ce15ec83dde7b2b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3dfecc1fa9d975e8eaaa306e97dd946677bf27")]
 [assembly: System.Reflection.AssemblyProductAttribute("domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
